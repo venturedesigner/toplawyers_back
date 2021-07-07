56e614648b76ff1937359a2d7bd270b6130902ef
devance <charlie@devance.co>
2021-07-07 15:49:14 +0100

    updateCurrentProfile

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -38,6 +38,52 @@ exports.updateProfile = (req, res) => {
     })
 }
 
+exports.updateCurrentProfile = (req, res) => {
+  console.log('Arrrgg')
+  let profileId = ''
+  userModel
+    .findById(res.locals.user._id)
+    // .populate('mychannels')
+    .then(user => {
+      profileId = user.profile
+      profileModel
+        .findById(profileId)
+        .then(profile => {
+          if (req.body.profilepic) { profile.profilepic = req.body.profilepic }
+          if (req.body.profilevideo) { profile.profilevideo = req.body.profilevideo }
+          if (req.body.description) { profile.description = req.body.description }
+          if (req.body.languages) { profile.languages = req.body.languages }
+          if (req.body.skills) { profile.skills = req.body.skills }
+          if (req.body.education) { profile.education = req.body.education }
+          if (req.body.experience) { profile.experience = req.body.experience }
+          if (req.body.linkedin) { profile.linkedin = req.body.linkedin }
+          if (req.body.twitter) { profile.twitter = req.body.twitter }
+          if (req.body.github) { profile.github = req.body.github }
+          if (req.body.tiktok) { profile.tiktok = req.body.tiktok }
+          if (req.body.instagram) { profile.instagram = req.body.instagram }
+
+          res.locals.profile = profile
+
+          profile.save()
+            .then(saved => {
+              res.status(200).json(res.locals.profile)
+            })
+            .catch(err => {
+              res.status(401).json({ msg: 'An error ocurred trying to save profile info', err })
+            })
+          
+        })
+        .catch(err => {
+          res.status(500).json({ msg: 'An error ocurred trying to update a profile5', err })
+        })
+
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'Error getting user', err })
+    })
+  
+}
+
 exports.getAllProfiles = (req, res) => {
   profileModel
     .find()
--- api/routes/profile.router.js
+++ api/routes/profile.router.js
@@ -3,13 +3,15 @@ const { checkAuth } = require('../../utils')
 const {
   updateProfile,
   getAllProfiles,
-  getCurrentProfile
+  getCurrentProfile,
+  updateCurrentProfile
 } = require('../controllers/profile.controller')
 
 const router = require('express').Router()
 
+router.get('/current', checkAuth, getCurrentProfile)
+router.post('/current', checkAuth, updateCurrentProfile)
 router.post('/:profileId', checkAuth, updateProfile)
 router.get('/', /* checkAuth, */ getAllProfiles)
-router.get('/current', checkAuth, getCurrentProfile)
 
 exports.profileRouter = router

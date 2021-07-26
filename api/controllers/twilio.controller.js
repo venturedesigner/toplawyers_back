87dbf217c0dd208daab1d726bf0ebcb158c16a23
CharlieMenu <43478341+elmenu@users.noreply.github.com>
2021-07-26 23:01:21 +0000

    fix bug tokenTwilio

--- api/controllers/twilio.controller.js
+++ api/controllers/twilio.controller.js
@@ -3,8 +3,8 @@ const AccessToken = require('twilio').jwt.AccessToken
 const VideoGrant = AccessToken.VideoGrant
 
 exports.getToken = (req, res) => {
-  const identity = res.locals.user._id
-  console.log('--> identity:', identity)
+  const identity = JSON.stringify(res.locals.user._id)
+  // console.log('--> identity:', identity, typeof identity)
   // var identity = req.query['identity'];
 
   if (!identity) {
@@ -15,7 +15,7 @@ exports.getToken = (req, res) => {
 
   // Create an access token which we will sign and return to the client,
   // containing the grant we just created.
-  const token = new AccessToken(
+  let token = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_SID,
     process.env.TWILIO_API_SECRET
@@ -25,15 +25,16 @@ exports.getToken = (req, res) => {
   token.identity = identity
 
   // Grant the access token Twilio Video capabilities.
-  const grant = new VideoGrant({
-    room: 'topdev'
-  })
+  const grant = new VideoGrant()
+  // const videoGrant = new VideoGrant({
+  //   room: 'topdev'
+  // })
   token.addGrant(grant)
 
   // Serialize the token to a JWT string and include it in a JSON response.
   console.log(token.toJwt())
 
-  res.status(200).send({
+  res.send({
     identity: identity,
     token: token.toJwt()
   })
